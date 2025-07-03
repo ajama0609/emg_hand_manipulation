144f51c428aa82230eef57bd2a9ace7b24a1a713
AJ <ajama0609@gmail.com>
2025-07-03 18:13:11 +0100

    model works now time to optimize!

--- EMG_classifier/Emg_model.py
+++ EMG_classifier/Emg_model.py
@@ -5,7 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import confusion_matrix  
 import seaborn as sns 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt  
+from imblearn.over_sampling import SMOTE
 class EMGClassfier(nn.Module): 
     def __init__(self,sequence_length,features,num_classes): 
         super().__init__()
@@ -25,7 +26,8 @@ class EMGClassfier(nn.Module):
             return logits,loss
         return logits 
     
-data = np.loadtxt('../features.csv',delimiter=',')  
+data = np.loadtxt('../features.csv',delimiter=',')   
+sm = SMOTE(random_state=42)
 X = data[:,:-1]  
 features=X.shape[1]
 sample = 1 
@@ -33,6 +35,7 @@ labels=data[:,-1]
 
 device = 'cuda:0'
 
+X, labels = sm.fit_resample(X, labels)
 X_train,X_test,labels_train,labels_test=train_test_split(X,labels,test_size=0.20,random_state=42)   
 X_test,X_valid,labels_test,labels_valid=train_test_split(X_test,labels_test,test_size=0.50,random_state=42)
 
@@ -44,12 +47,6 @@ labels_valid_tensor = torch.tensor(labels_valid, dtype=torch.int64, device=devic
 
 X_test_tensor = torch.tensor(X_test, dtype=torch.float32, device=device)
 labels_test_tensor = torch.tensor(labels_test, dtype=torch.int64, device=device) 
-
-# Create a TensorDataset from the tensors
-test_dataset = TensorDataset(X_test_tensor, labels_test_tensor)
-
-# Create DataLoader for test data
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 epochs=20
 lr=1e-3
@@ -76,31 +73,24 @@ for epoch in range(epochs):
 
     print(f"Epoch {epoch+1}/{epochs} | Train loss: {loss.item():.4f} | Train acc: {train_acc.item():.4f} | Val loss: {val_loss.item():.4f} | Val acc: {val_acc.item():.4f}")
 model.eval()   
-y_true=[] 
-y_pred=[]
 with torch.no_grad():  
-    for inputs,labels in test_loader: 
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-
-        outputs = model(inputs)
-        preds = torch.argmax(outputs, dim=1)
-
-        y_true.extend(labels.cpu().numpy())
-        y_pred.extend(preds.cpu().numpy())  
-
-y_true_tensor = torch.tensor(y_true)
-y_pred_tensor = torch.tensor(y_pred)
+    test_logits, test_loss = model(X_test_tensor, labels_test_tensor)
+    test_preds = torch.argmax(test_logits, dim=1)
+    test_acc = (test_preds == labels_test_tensor).float().mean() 
+y_true_tensor = torch.tensor(labels_test).to(device)
+y_pred_tensor = torch.tensor(test_preds).to(device)
 test_acc = (y_pred_tensor == y_true_tensor).float().mean()
 print(f"Test accuracy: {test_acc.item():.4f}")   
 
+y_true = torch.tensor(labels_test).cpu().numpy()
+y_pred = torch.tensor(test_preds).cpu().numpy()
 
 labels = np.unique(np.concatenate([y_true, y_pred]))
 
-cm = confusion_matrix(y_true,y_pred) 
+cm = confusion_matrix(y_true,y_pred,normalize='true') 
 
 plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues',
             xticklabels=labels, yticklabels=labels)
 plt.xlabel("Predicted")
 plt.ylabel("True")
